549b2a107e6464a47105b2f00024e24ec634677e
julioborn <juliobornes10@gmail.com>
2025-02-14 15:03:55 -0300

    pwd 1

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,14 @@ const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development", // 🔹 Desactiva PWA en desarrollo
 });
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // 🔹 Asegura que Next.js funcione bien con la App Router
+  },
 };
 
 export default withPWA(nextConfig);
